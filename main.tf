925730290224a5d0848d7c8bd84bba8d6a1c9789
Anibandisila0109 <162276852+Anibandisila0109@users.noreply.github.com>
2024-03-23 12:38:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "dev" {
     ami = "ami-02d7fd1c2af6eead0"
     instance_type = "t2.medium"
+    subnet_id = "subnet-02439af21c79fbab3"
     tags = {
       Name = "dev-ec2"
     }
